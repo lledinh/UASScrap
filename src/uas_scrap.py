bc49473ec2348f7b8939549c463a0b91725e3685
lam <ldl020590@yahoo.fr>
2020-11-08 12:18:25 +0100

    Initial commit

---
+++ src/uas_scrap.py
@@ -0,0 +1,103 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+import json
+from src.uas_scrap_sql import DBUASScrap
+
+
+class UASScrap:
+    ASSETS_LIST_URL = "https://assetstore.unity.com/?orderBy=1&page={}&rows=48";
+
+    def __init__(self) -> None:
+        self.index_pagination = 0
+        self.db_conn = DBUASScrap()
+        super().__init__()
+
+    @staticmethod
+    def get_asset_list_url(index_pagination):
+        return UASScrap.ASSETS_LIST_URL.format(index_pagination)
+
+    def add_categories(self, menu):
+        count = self.db_conn.get_count_categories()
+
+        if count == 0 or len(menu) != count:
+            self.db_conn.delete_categories()
+            for m in menu:
+                self.db_conn.insert_category(m)
+            self.db_conn.commit()
+
+    def add_publisher(self, publisher):
+        res = self.db_conn.get_publisher(publisher["publisherId"])
+
+        if len(res) == 0:
+            last_id = self.db_conn.insert_publisher(publisher)
+            self.db_conn.commit()
+            return last_id
+
+    def add_rating(self, rating):
+        last_id = self.db_conn.insert_rating(rating)
+        self.db_conn.commit()
+        return last_id
+
+    def add_asset(self, asset, id_publisher, id_rating, id_category):
+        last_id = self.db_conn.insert_asset(asset, id_publisher, id_rating, id_category)
+        self.db_conn.commit()
+        return last_id
+
+    def get_categories(self, categories, menu):
+        for c in categories:
+            t = (c['id'], c['name'], c['slug'])
+            menu.append(t)
+
+            if 'subs' in c:
+                self.get_categories(c['subs'], menu)
+
+    def get_asset_list(self):
+        session = requests.Session()
+        request = session.get(self.get_asset_list_url(self.index_pagination))
+        soup = BeautifulSoup(request.text, 'html.parser')
+        print(session.cookies.get_dict())
+
+        sjson = ""
+        for script in soup.find_all("script"):
+            if "DOMContentLoaded" in str(script):
+                regex = "ReactDOMrender\\(({.*}),document.getElementById"
+                res = re.search(regex, str(script))
+                sjson = json.loads(res.group(1))
+
+        json_product = sjson["data"]["ENTITY"]["ProductQ"]
+
+        print(len(json_product))
+        for key in json_product:
+            print(json_product[key])
+            publisher = {
+                "publisherId": json_product[key]["publisherId"],
+                "publisherName": json_product[key]["publisherName"]
+            }
+            rating = {
+                "average": json_product[key]["rating"]["average"],
+                "count": json_product[key]["rating"]["count"]
+            }
+            last_id_publisher = self.add_publisher(publisher)
+            last_id_rating = self.add_rating(rating)
+
+            asset = {
+                "idAsset": json_product[key]["id"], "name": json_product[key]["name"],
+                "mainImage": json_product[key]["mainImage"], "price": json_product[key]["price"]["price"],
+                "originPrice": json_product[key]["price"]["originPrice"]
+            }
+
+            self.add_asset(asset, last_id_publisher, last_id_rating, 0)
+
+        menu = []
+        self.get_categories(sjson["data"]["filter"]["defaults"]['category']['options'], menu)
+        self.add_categories(menu)
+
+
+def main():
+    scrap = UASScrap()
+    scrap.get_asset_list()
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/uas_scrap_sql.py
@@ -0,0 +1,89 @@
+import mysql.connector
+
+
+class DBUASScrap:
+    host = "192.168.1.51"
+    user = "lam"
+    password = "ldl",
+    database = "uas_scrap"
+
+    def __init__(self) -> None:
+        self.conn = mysql.connector.connect(
+            host="192.168.1.51",
+            user="lam",
+            password="ldl",
+            database="uas_scrap"
+        )
+        super().__init__()
+
+    def insert_category(self, category):
+        cursor = self.conn.cursor()
+        sql = "INSERT INTO UnityAssetCategory (name, slug, idCategory) VALUES (%s, %s, %s)"
+        val = (category[1], category[2], category[0])
+        cursor.execute(sql, val)
+
+        return cursor.lastrowid
+
+    def insert_asset(self, asset, publisher_id, rating_id, category_id):
+        cursor = self.conn.cursor()
+        sql = "INSERT INTO " \
+              "Asset (idPublisher, name, mainImage, price, originPrice, idRating, idPublisher, id_Category)" \
+              " VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+        val = (asset["idAsset"], asset["name"], asset["mainImage"],
+               asset["price"], asset["originPrice"], rating_id, publisher_id, category_id)
+
+        cursor.execute(sql, val)
+
+        return cursor.lastrowid
+
+    def insert_publisher(self, publisher):
+        cursor = self.conn.cursor()
+        sql = "INSERT INTO Publisher (idPublisher, name) VALUES (%s, %s)"
+        val = (publisher["publisherId"], publisher["publisherName"])
+        cursor.execute(sql, val)
+
+        return cursor.lastrowid
+
+    def insert_rating(self, rating):
+        cursor = self.conn.cursor()
+        sql = "INSERT INTO Rating (rating, count) VALUES (%s, %s)"
+        val = (rating["average"], rating["count"])
+        cursor.execute(sql, val)
+
+        return cursor.lastrowid
+
+    def get_publisher(self, id_publisher):
+        cursor = self.conn.cursor()
+        sql = "SELECT * FROM Publisher WHERE idPublisher = {}".format(id_publisher)
+        cursor.execute(sql)
+        results = cursor.fetchall()
+
+        return results
+
+    def get_rating(self, id_asset):
+        cursor = self.conn.cursor()
+        sql = "SELECT * FROM Asset JOIN Rating ON Asset.idRating = Rating.id WHERE Asset.idAsset = {}".format(id_asset)
+        cursor.execute(sql)
+        results = cursor.fetchall()
+
+        return results
+
+    def get_count_categories(self):
+        cursor = self.conn.cursor()
+        sql = "SELECT COUNT(*) FROM UnityAssetCategory"
+        cursor.execute(sql)
+        results = cursor.fetchall()
+
+        count = 0
+        for r in results:
+            count = r
+
+        return count
+
+    def delete_categories(self):
+        cursor = self.conn.cursor()
+        sql = "DELETE FROM UnityAssetCategory"
+        cursor.execute(sql)
+
+    def commit(self):
+        self.conn.commit()
